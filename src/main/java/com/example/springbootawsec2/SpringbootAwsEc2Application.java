eb87638a4fe5a1bf64cd181be1247550251fda43
Kiruthika <kiruthikas98@gmail.com>
2023-12-27 15:58:36 +0530

    first commit

---
+++ src/main/java/com/example/springbootawsec2/SpringbootAwsEc2Application.java
@@ -0,0 +1,20 @@
+package com.example.springbootawsec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootAwsEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsEc2Application.class, args);
+	}
+	
+	@GetMapping("/")
+	public String getData() {
+		return "HELLO KIRUTHIKA";
+	}
+}
