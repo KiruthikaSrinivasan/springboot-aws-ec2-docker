44abea9e1f3b160042c728eea4fdb64b6b04bc5b
KiruthikaSrinivasan <132181034+KiruthikaSrinivasan@users.noreply.github.com>
2024-01-03 13:52:28 +0530

    Update SpringbootAwsEc2Application.java
    second change

--- src/main/java/com/example/springbootawsec2/SpringbootAwsEc2Application.java
+++ src/main/java/com/example/springbootawsec2/SpringbootAwsEc2Application.java
@@ -15,6 +15,6 @@ public class SpringbootAwsEc2Application {
 	
 	@GetMapping("/")
 	public String getData() {
-		return "HELLO KIRUTHIKA SRINIVASAN FROM AMIZHTH TECH";
+		return "HELLO KIRUTHIKA SRINIVASAN FROM AMIZHTH TECHNOLOGY";
 	}
 }
