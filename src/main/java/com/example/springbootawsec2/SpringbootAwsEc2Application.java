f589c575c6cbb518991eaf230bb2b95ceacdddb1
KiruthikaSrinivasan <132181034+KiruthikaSrinivasan@users.noreply.github.com>
2026-06-24 21:29:13 +0300

    Update SpringbootAwsEc2Application.java

--- src/main/java/com/example/springbootawsec2/SpringbootAwsEc2Application.java
+++ src/main/java/com/example/springbootawsec2/SpringbootAwsEc2Application.java
@@ -15,6 +15,6 @@ public class SpringbootAwsEc2Application {
 	
 	@GetMapping("/")
 	public String getData() {
-		return "HELLO KIRUTHIKA S---> Hi Jenkins after new update";
+		return "HELLO KIRUTHIKA S---> Hi Jenkins after New Update";
 	}
 }
